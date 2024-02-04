3ea46c9a77848862760d1ae2cc97751a7404c50b
AnisDhia <50758880+AnisDhia@users.noreply.github.com>
2024-02-04 12:20:10 +0100

    Add Svg Icons

---
+++ src/components/icons/apartment.vue
@@ -0,0 +1,13 @@
+<template>
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M14 8V2C14 0.9 13.1 0 12 0H6C4.9 0 4 0.9 4 2V4H2C0.9 4 0 4.9 0 6V16C0 17.1 0.9 18 2 18H8V14H10V18H16C17.1 18 18 17.1 18 16V10C18 8.9 17.1 8 16 8H14ZM4 16H2V14H4V16ZM4 12H2V10H4V12ZM4 8H2V6H4V8ZM8 12H6V10H8V12ZM8 8H6V6H8V8ZM8 4H6V2H8V4ZM12 12H10V10H12V12ZM12 8H10V6H12V8ZM12 4H10V2H12V4ZM16 16H14V14H16V16ZM16 12H14V10H16V12Z"
+            fill="#888B97" />
+    </svg>
+</template>
+
+<script>
+export default {
+    name: "HouseSvg",
+};
+</script>
